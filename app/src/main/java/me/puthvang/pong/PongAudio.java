9b4ae7e2409fc2056f05aebb7818c4d1c4920fd0
Puth Vang <35241506+PuthVang@users.noreply.github.com>
2023-09-15 19:04:49 -0700

    Added in background music!

--- app/src/main/java/me/puthvang/pong/PongAudio.java
+++ app/src/main/java/me/puthvang/pong/PongAudio.java
@@ -24,46 +24,69 @@ public class PongAudio {
     private int mBopID = -1;
     private int mMissID = -1;
 
+    // This is for playing the background music
+
+    private MediaPlayer mediaPlayer;
+
     public PongAudio(Context context) {
         this.context = context;
 
-        // Prepare the SoundPool instance
-        // Depending upon the version of Android
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .build();
-
-            mSP = new SoundPool.Builder()
-                    .setMaxStreams(10)
-                    .setAudioAttributes(audioAttributes)
-                    .build();
-        } else mSP = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
-
-        // Open each of the sound files in turn
-        // and load them in to Ram ready to play
-        // The try-catch blocks handle when this fails
-        // and is required.
-
         try {
-            AssetManager assetManager = this.context.getAssets();
-            AssetFileDescriptor descriptor;
 
-            descriptor = assetManager.openFd("beep.ogg");
-            mBeepID = mSP.load(descriptor, 0);
+            this.mediaPlayer = new MediaPlayer();
+            AssetFileDescriptor afd = context.getAssets().openFd("The_Infinite_Race.ogg");
+            this.mediaPlayer.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+            afd.close();
+            this.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            this.mediaPlayer.prepare();
+            this.mediaPlayer.setVolume(1f, 1f);
+            this.mediaPlayer.setLooping(true);
 
-            descriptor = assetManager.openFd("boop.ogg");
-            mBoopID = mSP.load(descriptor, 0);
+            if(!this.mediaPlayer.isPlaying()) {
+                bg();
+            }
 
-            descriptor = assetManager.openFd("bop.ogg");
-            mBopID = mSP.load(descriptor, 0);
+            // Prepare the SoundPool instance
+            // Depending upon the version of Android
 
-            descriptor = assetManager.openFd("miss.ogg");
-            mMissID = mSP.load(descriptor, 0);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                        .build();
+
+                mSP = new SoundPool.Builder()
+                        .setMaxStreams(10)
+                        .setAudioAttributes(audioAttributes)
+                        .build();
+            } else mSP = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+
+            // Open each of the sound files in turn
+            // and load them in to Ram ready to play
+            // The try-catch blocks handle when this fails
+            // and is required.
+
+            try {
+                AssetManager assetManager = this.context.getAssets();
+                AssetFileDescriptor descriptor;
+
+                descriptor = assetManager.openFd("beep.ogg");
+                mBeepID = mSP.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("boop.ogg");
+                mBoopID = mSP.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("bop.ogg");
+                mBopID = mSP.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("miss.ogg");
+                mMissID = mSP.load(descriptor, 0);
+            } catch (IOException e) {
+                Log.e("error", "failed to load sound files");
+            }
+
         } catch (IOException e) {
-            Log.e("error", "failed to load sound files");
+            Log.e("error", "failed to load background music files");
         }
 
     }
@@ -82,6 +105,14 @@ public class PongAudio {
 
     public void bop() {
         mSP.play(mBopID, 1, 1, 0, 0, 1);
+    }
+
+    public void bg() {
+        this.mediaPlayer.start();
+    }
+
+    public void bgPause() {
+        this.mediaPlayer.pause();
     }
 
 }
